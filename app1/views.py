79b4819ce923a80396b16b5694ae65337ab43655
karimPhatan7 <mrd313888@gmail.com>
2023-11-08 11:25:11 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import *
+
+def karim(request):
+    return HttpResponse('<h1><marquee>Hai Karim How are you</h1></marquee>')
+def phatan(request):
+    return HttpResponse('<h1 style=bg-color:"yellow"><marquee>Iam good What about you</h1></marquee>')
+
+
